a81b821b8476263a9f6e2803b9db622bb94a5fa9
Yonatan Greenfeld <yonathan@jit.io>
2022-05-09 10:20:31 +0300

    Remove vulnerabilities

--- file.go
+++ file.go
@@ -3,7 +3,6 @@ package main
 
 
 import (
-    "github.com/dgrijalva/jwt-go"
 	"context"
 
 	"github.com/rclone/rclone/cmd"
--- py.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-a = 2
-
-subprocess.Popen(['echo', 'hello'], shell=True)
